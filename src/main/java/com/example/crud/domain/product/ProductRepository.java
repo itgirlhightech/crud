92cefa7ff85565cc373755b06afa7c18843cb17e
Evilyn Feitosa <evy123feitosa@gmail.com>
2024-10-11 16:25:38 -0300

    first commit

---
+++ src/main/java/com/example/crud/domain/product/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.crud.domain.product;
+
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface ProductRepository extends JpaRepository<Product, String > {
+  List<Product> findAllByActiveTrue();
+}
